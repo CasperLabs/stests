61afeb4aca5268542e8a376b7522760cb55b1efd
Mark A. Greenslade <mark@casperlabs.io>
2020-01-31 15:21:56 +0100

    1. Updated stack-update command.

--- sh/stack_update.sh
+++ sh/stack_update.sh
@@ -8,7 +8,8 @@ main()
 {
     log "stack update :: starts ..."
 
-    update_source
+    # update_source
+    update_env_vars
 
     log "stack update :: complete"
 }
@@ -19,6 +20,19 @@ update_source()
     log "stack update :: pull repo updates"
     cd $STESTS_HOME
     git pull
+}
+
+# Update environment variables.
+update_env_vars()
+{
+	if [ -f $HOME/.stests ]; then
+		declare backup=$HOME/.stests-"`date +%Y-%m-%d`"
+		cp $HOME/.stests $backup
+		log "stack update :: env vars file backup --> "$backup
+	fi
+
+    cp $STESTS_TEMPLATES/stests_env.sh $HOME/.stests
+    log "stack update :: new env vars file --> "$HOME/.stests
 }
 
 # Invoke entry point.
