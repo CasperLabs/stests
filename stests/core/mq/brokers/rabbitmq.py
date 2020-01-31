2bc89c494e97c856764e7d72b79fa36b10c808c7
Mark A. Greenslade <mark@casperlabs.io>
2020-01-31 14:38:14 +0100

    1. Fixed env var name typos.

--- stests/core/mq/brokers/rabbitmq.py
+++ stests/core/mq/brokers/rabbitmq.py
@@ -7,25 +7,25 @@ from stests.core.utils import env
 # Environment variables required by this module.
 class EnvVars:
     # RabbitMQ host.
-    HOST = env.get_var('MQ_BROKER_RABBIT_HOST', "localhost")
+    HOST = env.get_var('BROKER_RABBIT_HOST', "localhost")
 
     # RabbitMQ port.
-    PORT = env.get_var('MQ_BROKER_RABBIT_PORT', 5672, int)
+    PORT = env.get_var('BROKER_RABBIT_PORT', 5672, int)
 
     # RabbitMQ protocol.
-    PROTOCOL = env.get_var('MQ_BROKER_RABBIT_PROTOCOL', "amqp")
+    PROTOCOL = env.get_var('BROKER_RABBIT_PROTOCOL', "amqp")
 
     # RabbitMQ SSL client cert.
-    SSL_CLIENT_CERT = env.get_var('MQ_BROKER_RABBIT_SSL_CLIENT_CERT')
+    SSL_CLIENT_CERT = env.get_var('BROKER_RABBIT_SSL_CLIENT_CERT')
 
     # RabbitMQ SSL client cert key.
-    SSL_CLIENT_KEY = env.get_var('MQ_BROKER_RABBIT_SSL_CLIENT_KEY')
+    SSL_CLIENT_KEY = env.get_var('BROKER_RABBIT_SSL_CLIENT_KEY')
 
     # RabbitMQ user.
-    USER = env.get_var('MQ_BROKER_RABBIT_USER', "stests-mq-user")
+    USER = env.get_var('BROKER_RABBIT_USER', "stests-mq-user")
 
     # RabbitMQ user password.
-    USER_PWD = env.get_var('MQ_BROKER_RABBIT_USER_PWD', "clabs")
+    USER_PWD = env.get_var('BROKER_RABBIT_USER_PWD', "clabs")
 
 
 
--- stests/core/mq/brokers/redis.py
+++ stests/core/mq/brokers/redis.py
@@ -7,13 +7,13 @@ from stests.core.utils import env
 # Environment variables required by this module.
 class EnvVars:
     # RabbitMQ host.
-    DB = env.get_var('MQ_BROKER_REDIS_DB', 0, int)
+    DB = env.get_var('BROKER_REDIS_DB', 0, int)
 
     # RabbitMQ protocol.
-    HOST = env.get_var('MQ_BROKER_REDIS_HOST', "amqp")
+    HOST = env.get_var('BROKER_REDIS_HOST', "amqp")
 
     # RabbitMQ port.
-    PORT = env.get_var('MQ_BROKER_REDIS_PORT', 5672, int)
+    PORT = env.get_var('BROKER_REDIS_PORT', 5672, int)
 
 
 def get_broker(network_id: str) -> RedisBroker:
