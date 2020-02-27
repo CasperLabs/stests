ede4afb0b59e656a59575d506814a692edd9ec27
Mark A. Greenslade <mark@casperlabs.io>
2020-02-27 17:05:02 +0100

    1. Added commands to update network/node states.

---
+++ stests/cli/cache/set_network_status.py
@@ -0,0 +1,53 @@
+import argparse
+
+from stests.core import cache
+from stests.core.domain import NetworkStatus
+from stests.core.utils import args_validator
+from stests.core.utils import defaults
+from stests.core.utils import factory
+from stests.core.utils import logger
+
+
+# CLI argument parser.
+ARGS = argparse.ArgumentParser("Informs stests of a mutation in the status of a network.")
+
+# CLI argument: network name.
+ARGS.add_argument(
+    "network",
+    help="Network name {type}{id}, e.g. lrt1.",
+    type=args_validator.validate_network
+    )
+
+# Set CLI argument: network status.
+ARGS.add_argument(
+    "status",
+    choices=[i.name.lower() for i in NetworkStatus],
+    help="Network status.",
+    type=str
+    )
+
+
+def main(args):
+    """Entry point.
+    
+    :param args: Parsed CLI arguments.
+
+    """
+    # Pull.
+    network = cache.get_network_by_name(args.network)
+    if network is None:
+        raise ValueError("Unregistered network.")
+
+    # Update.
+    network.status = NetworkStatus[args.status.upper()]
+
+    # Push.
+    cache.set_network(network)
+
+    # Notify.
+    logger.log(f"Network {args.network} status was updated --> {network.status}")
+
+
+# Entry point.
+if __name__ == '__main__':
+    main(ARGS.parse_args())
