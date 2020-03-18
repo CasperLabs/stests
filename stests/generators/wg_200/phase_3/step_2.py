e2b6c5d5515022851d4ecc0cbc655d1dab15420c
Mark A. Greenslade <mark@casperlabs.io>
2020-03-18 18:33:49 +0100

    1. Step 2 of counter-call..

---
+++ stests/generators/wg_200/phase_3/step_2.py
@@ -0,0 +1,44 @@
+from stests.core.orchestration import ExecutionContext
+from stests.generators import utils
+from stests.generators.wg_200 import constants
+
+
+
+# Step description.
+DESCRIPTION = "Refunds funds previously transferred from network faucet."
+
+# Step label.
+LABEL = "refund-network-faucet"
+
+
+def execute(ctx: ExecutionContext):
+    """Step entry point.
+    
+    :param ctx: Execution context information.
+
+    """     
+    utils.do_refund.send(
+        ctx,
+        constants.ACC_RUN_FAUCET,
+        constants.ACC_NETWORK_FAUCET
+    )
+
+
+def verify(ctx: ExecutionContext):
+    """Step verifier.
+    
+    :param ctx: Execution context information.
+
+    """
+    utils.verify_deploy_count(ctx, 1)
+
+
+def verify_deploy(ctx: ExecutionContext, dhash: str):
+    """Step deploy verifier.
+    
+    :param ctx: Execution context information.
+    :param dhash: A deploy hash.
+
+    """
+    utils.verify_deploy(ctx, dhash)
+    utils.verify_refund(ctx, dhash)
