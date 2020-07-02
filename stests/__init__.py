888fec23ec07bbb38529db63c351cfda7663c77b
Mark A. Greenslade <mark@casperlabs.io>
2020-07-03 00:19:04 +0200

    1. Added license.

--- stests/__init__.py
+++ stests/__init__.py
@@ -7,7 +7,7 @@
 #    ╚═════╝░░░░╚═╝░░░╚══════╝╚═════╝░░░░╚═╝░░░╚═════╝░
 
 __title__ = 'stests'
-__version__ = '0.4.0'
+__version__ = '0.4.1'
 __author__ = 'Casper Labs AG, Zug, Switzerland'
 __license__ = 'CasperLabs Open Source License (COSL)'
 __copyright__ = 'Copyright 2020 Casper Labs'
