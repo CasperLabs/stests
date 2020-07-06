0fd494c2a8acf815a7d15becd8df42e2ec4dc67f
Mark A. Greenslade <mark@casperlabs.io>
2020-07-06 15:13:37 +0200

    1. Auto-flushed cache prior to network registration.

---
+++ sh/cache/register.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Import utils.
+source $STESTS_PATH_SH/utils.sh
+
+# Main entry point.
+function main()
+{
+    # Flush cache.
+    source $STESTS_PATH_SH/cache/flush.sh
+    source $STESTS_PATH_SH/cache/flush_infra.sh
+
+    pushd $STESTS_HOME
+    pipenv run python $STESTS_PATH_SH_SCRIPTS/register.py
+    popd -1    
+}
+
+# Invoke entry point.
+main
