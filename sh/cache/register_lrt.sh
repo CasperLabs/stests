a7041b228c926808eb612f27a18daf0e64fe4723
Mark A. Greenslade <mark@casperlabs.io>
2020-10-03 15:09:18 +0200

    1. Updated various commands to take explicit params.

---
+++ sh/cache/register_lrt.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Import utils.
+source $STESTS_PATH_SH/utils.sh
+
+# Main entry point.
+function main()
+{
+    log "sre :: registering lrt assets ..."
+
+    # Flush cache.
+    source $STESTS_PATH_SH/cache/flush.sh
+    source $STESTS_PATH_SH/cache/flush_infra.sh
+
+    pushd $STESTS_HOME
+    pipenv run python $STESTS_PATH_SH_SCRIPTS/cache_register_lrt.py
+    popd -1    
+
+    log "sre :: registered lrt assets"
+}
+
+# Invoke entry point.
+main
