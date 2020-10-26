0b88dbcbb22bcd590ce1d85fe8dd7eada693dcd6
Mark A. Greenslade <mark@casperlabs.io>
2020-10-26 23:02:54 +0100

    1. Version bump.

--- stests/__init__.py
+++ stests/__init__.py
@@ -7,7 +7,7 @@
 #    ╚═════╝░░░░╚═╝░░░╚══════╝╚═════╝░░░░╚═╝░░░╚═════╝░
 
 __title__ = 'stests'
-__version__ = '0.5.3'
+__version__ = '0.6.0'
 __author__ = 'Casper Labs AG, Zug, Switzerland'
 __license__ = 'CasperLabs Open Source License (COSL)'
 __copyright__ = 'Copyright 2020 Casper Labs'
