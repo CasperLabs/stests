1cd3610222aa00e1c78f2b92858474a4302e1559
Mark LeMoine <linclelinkpart5@gmail.com>
2021-03-13 23:27:02 -0800

    Try updated import style to fix relative import errors

--- sh/scripts/node_stop.py
+++ sh/scripts/node_stop.py
@@ -8,10 +8,10 @@ from stests.core.utils import args_validator
 from stests.core.utils import cli as utils
 from stests.core.utils import env
 from stests.core.types.infra import Node
-from .arg_utils import get_network_node
-from .svc_utils import get_arg_parser
-from .svc_utils import remote_node_systemctl
-from .svc_utils import SvcCommand
+from sh.scripts.arg_utils import get_network_node
+from sh.scripts.svc_utils import get_arg_parser
+from sh.scripts.svc_utils import remote_node_systemctl
+from sh.scripts.svc_utils import SvcCommand
 
 SVC_COMMAND = SvcCommand.STOP
 
