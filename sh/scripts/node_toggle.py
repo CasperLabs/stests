12c037373a5cb553881c8be66a4d8ffce55d8064
Mark LeMoine <linclelinkpart5@gmail.com>
2021-03-15 23:13:50 -0700

    Test code for `node_toggle`

--- sh/scripts/node_toggle.py
+++ sh/scripts/node_toggle.py
@@ -1,5 +1,21 @@
+from sh.scripts.arg_utils import get_network_node
+from sh.scripts.svc_utils import get_arg_parser
 from sh.scripts.svc_utils import get_healthy_nodes
+from sh.scripts.svc_utils import remote_node_systemctl
 from sh.scripts.svc_utils import SvcCommand
 
+SVC_COMMAND = SvcCommand.STOP
+
+def main(args):
+    """Entry point.
+
+    :param args: Parsed CLI arguments.
+
+    """
+    network, node = get_network_node(args)
+
+    get_healthy_nodes(network)
+
 if __name__ == '__main__':
-    get_healthy_nodes('lrt01')
+    parser = get_arg_parser(SVC_COMMAND)
+    main(parser.parse_args())
