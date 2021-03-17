0911d6b1a76ecfaf20edd63aecdfe421721bb9d3
Mark LeMoine <linclelinkpart5@gmail.com>
2021-03-16 23:19:46 -0700

    Add editing of config files

--- sh/scripts/node_upgrade.py
+++ sh/scripts/node_upgrade.py
@@ -15,46 +15,43 @@ class Semver(tp.NamedTuple):
     minor: int
     patch: int
 
-    def __str__(self) -> str:
+    def snake_str(self) -> str:
         return f'{self.major}_{self.minor}_{self.patch}'
 
-def push_dummy_update_to_node(
+    def dotted_str(self) -> str:
+        return f'{self.major}.{self.minor}.{self.patch}'
+
+def push_update_to_node(
     ssh_user: str,
     ssh_host: str,
     ssh_key_path: pl.Path,
     semver: Semver,
     local_bin_repo_dir: pl.Path,
     local_cfg_repo_dir: pl.Path,
+    activation_era: int,
     public_address: str=None,
     public_port: int=35000,
+    remote_bin_repo_dir: pl.Path=pl.Path('/var/lib/casper/bin'),
+    remote_cfg_repo_dir: pl.Path=pl.Path('/etc/casper'),
 ):
     if not public_address:
         utils.log(f'Using SSH address as public address ({ssh_host})')
         public_address = ssh_host
 
-    semver_str = str(semver)
+    semver_snake_str = semver.snake_str()
+    semver_dotted_str = semver.dotted_str()
 
-    local_bin_dir = local_bin_repo_dir / semver_str
-    local_cfg_dir = local_cfg_repo_dir / semver_str
+    local_bin_dir = local_bin_repo_dir / semver_snake_str
+    local_cfg_dir = local_cfg_repo_dir / semver_snake_str
 
     # Construct the expected remote repo paths to copy the new version dirs to.
     # A "repo dir" is a directory that contains subdirs of the form "X_Y_Z",
     # where X, Y, Z are major, minor, patch numbers, respectively.
-    remote_bin_repo_dir = pl.Path('/var/lib/casper/bin')
-    remote_cfg_repo_dir = pl.Path('/etc/casper')
-
     utils.log(f'Remote target bin repo dir `{remote_bin_repo_dir}`')
     utils.log(f'Remote target cfg repo dir `{remote_cfg_repo_dir}`')
 
-    # Check that the local cache has the files we expect to see.
-    expected_files = (
-        'casper-node',
-        'chainspec.toml',
-        'config-example.toml',
-        'casper-node-launcher-state.toml',
-    )
-
-    # Copy over casper-node binary.
+    # Copy over casper-node binary dir.
+    utils.log('Copying over casper-node binary')
     remote_node_ssh_copy(
         source_path=local_bin_dir,
         ssh_user=ssh_user,
@@ -62,6 +59,55 @@ def push_dummy_update_to_node(
         target_dir=remote_bin_repo_dir,
         ssh_key_path=ssh_key_path,
     )
+
+    # Need to edit some config files, so create a temp dir.
+    with tempfile.TemporaryDirectory() as tmp_dir:
+        tmp_dir = pl.Path(tmp_dir)
+
+        tmp_semver_dir = tmp_dir / semver_snake_str
+        tmp_semver_dir.mkdir()
+
+        # Edit node config.
+        utils.log('Customizing `config.toml`')
+        config_toml = toml.load(local_cfg_dir / 'config-example.toml')
+
+        config_toml['network']['public_address'] = f'{public_address}:{public_port}'
+
+        with (tmp_semver_dir / 'config.toml').open('w') as fp:
+            toml.dump(config_toml, fp)
+
+        # Edit launcher state.
+        utils.log('Customizing `casper-node-launcher-state.toml`')
+        launcher_state_toml = toml.load(local_cfg_dir / 'casper-node-launcher-state.toml')
+
+        launcher_state_toml['version'] = semver_dotted_str
+        launcher_state_toml['binary_path'] = str(remote_bin_repo_dir / semver_snake_str / 'casper-node')
+        launcher_state_toml['config_path'] = str(remote_cfg_repo_dir / semver_snake_str / 'config.toml')
+
+        with (tmp_semver_dir / 'casper-node-launcher-state.toml').open('w') as fp:
+            toml.dump(launcher_state_toml, fp)
+
+        # Edit chainspec.
+        utils.log('Customizing `chainspec.toml`')
+        chainspec_toml = toml.load(local_cfg_dir / 'chainspec.toml')
+
+        chainspec_toml['protocol']['version'] = semver_dotted_str
+        chainspec_toml['protocol']['activation_point'] = activation_era
+
+        with (tmp_semver_dir / 'chainspec.toml').open('w') as fp:
+            toml.dump(chainspec_toml, fp)
+
+        # Copy over modified configs.
+        utils.log('Copying over modified configs')
+        remote_node_ssh_copy(
+            source_path=tmp_semver_dir,
+            ssh_user=ssh_user,
+            ssh_host=ssh_host,
+            target_dir=remote_cfg_repo_dir,
+            ssh_key_path=ssh_key_path,
+        )
+
+    # TODO: Continue here.
 
 def get_arg_parser() -> argparse.ArgumentParser:
     parser = argparse.ArgumentParser(
