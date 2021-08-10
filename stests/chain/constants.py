de510c25566f148289f601d2e5186037aaa07634
Mateusz Górski <goral09@users.noreply.github.com>
2021-08-10 19:13:26 +0200

    Increase default txn cost.
    Recently a cost of purse creation has increased and the default payment is not enough to cover the execution.

--- stests/chain/constants.py
+++ stests/chain/constants.py
@@ -5,7 +5,7 @@ DEFAULT_TX_TIME_TO_LIVE = "3600000ms"
 DEFAULT_TX_GAS_PRICE = 10
 
 # Default transaction fee to apply.
-DEFAULT_TX_FEE = int(1e10)
+DEFAULT_TX_FEE = int(1e11)
 
 # Default transaction fee for native transfers.
 DEFAULT_TX_FEE_NATIVE_TRANSFER = int(1e4)
